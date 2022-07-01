89f4d9838ebb5c40557eb8d354dd59520d21c283
Alejandro Lopez <alzcol@outlook.com>
2022-07-01 13:33:18 -0400

    Add project files.

---
+++ HubspotWebAnalytics/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using HubspotWebAnalytics.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
+
+namespace HubspotWebAnalytics.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private  IConfiguration _configuration { get; }
+        //Destination API key
+        public static string DestinationAPIKey = "";
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration IConfig)
+        {
+            _logger = logger;
+            _configuration = IConfig;
+            DestinationAPIKey = _configuration.GetSection("HubspotWebAnalyticsSettings").GetSection("DestinationAPIKey").Value;
+        }
+
+        //These are the URI(s) that contain the API key for Contacts
+        public static string initContactv3 = "https://api.hubapi.com/crm/v3/objects/contacts?hapikey=" + DestinationAPIKey;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateContact(HubspotContactModel Contact)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var myContent = JsonConvert.SerializeObject(Contact,
+                    new JsonSerializerSettings()
+                    {
+                        NullValueHandling = NullValueHandling.Ignore
+                    });
+                    var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+                    var byteContent = new ByteArrayContent(buffer);
+                    byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                    client.BaseAddress = new Uri(initContactv3);
+                    client.DefaultRequestHeaders.Accept.Clear();
+
+                    var response = client.PostAsync("", byteContent).Result;
+                    var readTask = response.Content.ReadAsStringAsync().Result;
+
+                    //GET Method
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string success = (response.ReasonPhrase
+                                + ": "
+                                + "Successfully Created the Contact: " + Contact.firstname + " " + Contact.lastname);
+                        _logger.LogTrace(success);
+                    }
+                    else
+                    {
+                        string fail = (response.ReasonPhrase
+                                + ": "
+                                + "Failed to Created the Contact: " + Contact.firstname + " " + Contact.lastname);
+                        _logger.LogTrace(fail);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                string fail = ("Failed to Created the Contact: " + Contact.firstname + " " + Contact.lastname);
+                _logger.LogTrace(fail);
+            }
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HubspotWebAnalytics/Models/HubspotContactModel.cs
@@ -0,0 +1,14 @@
+﻿namespace HubspotWebAnalytics.Models
+{
+    public class HubspotContactModel
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string hs_lead_status { get; set; }
+        public string lifecyclestage { get; set; }
+        public string email { get; set; }
+        public string userid { get; set; }
+        public string phonenumber { get; set; }
+        public string company { get; set; }
+    }
+}
